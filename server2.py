28db024bb6504fc120e7263a141c58ff27da66a9
Jay Shah <61103636+JS12540@users.noreply.github.com>
2024-06-17 16:46:50 +0530

    Add files via upload

---
+++ server2.py
@@ -0,0 +1,11 @@
+# server2.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello from Server 2!'
+
+if __name__ == '__main__':
+    app.run(port=5002)
